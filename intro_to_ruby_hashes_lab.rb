0253dbc931bac9a0f5db8a32f03a13b975a962f4
Brian Kao <bk1613@nyu.edu>
2019-09-04 18:45:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,7 +13,7 @@ def monopoly_with_second_tier
   # complexity.
   hashheroes = {
     "Avengers" => {
-      
+      :name => "iron Man"
     }
   }
 end
@@ -24,7 +24,9 @@ def monopoly_with_third_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   hashheroes = {
-    "Avengers" => {}
+    "Avengers" => {
+      
+    }
   }
 end
 
