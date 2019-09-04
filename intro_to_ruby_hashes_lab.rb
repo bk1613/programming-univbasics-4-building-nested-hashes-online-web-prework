3f16a09fee767cf5765252149df9b1675bcd8517
Brian Kao <bk1613@nyu.edu>
2019-09-04 19:24:10 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -49,27 +49,19 @@ def monopoly_with_fourth_tier
   # complexity.
   hashrailroad = {
     :railroads => {
-			:rent_in_dollars => {
-				four_pieces_owned: 200,
-				three_pieces_owned: 100,
-				two_pieces_owned: 50,
-				one_piece_owned: 25
-			},
-			names: {
-				reading_railroad: {
-					"mortgage_value" => "$100"
-				},
-				pennsylvania_railroad: {
-					"mortgage_value" => "$200"
-				},
-				b_and_o_railroad: {
-					"mortgage_value" => "$400"
-				},
-				shortline_railroad: {
-					"mortgage_value" => "$800"
-				}
-			},
-
-		}
-	}
+      :members => {
+        :first_hero => "iron Man",
+        :second_hero => "captain America",
+        :third => "Thor",
+        :fourth => "hulk"
+      },
+      
+      :powers => {
+        :iron_avenger => {:power => "energy beam"},
+        :super_soldier => {:power => "shield"},
+        :god => {:power => "thunder"},
+        :gamma_bomb => {:power => "stregnth"}
+      },
+    }
+  }
 end
