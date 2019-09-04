dde7560cee4d45562a48f3da15cc42a11401e354
Brian Kao <bk1613@nyu.edu>
2019-09-04 19:16:28 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,9 @@
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-	hash = {
-		:railroads => {}
-	}
+	hashheroes = {
+    "Avengers" => {}
+  }
 end
 
 def monopoly_with_second_tier
@@ -11,11 +11,11 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-  hash = {
-		:railroads => {
-			:pieces => 4
-		}
-	}
+  hashheroes = {
+    "Avengers" => {
+      :name => "iron Man"
+    }
+  }
 end
 
 def monopoly_with_third_tier
@@ -30,14 +30,14 @@ def monopoly_with_third_tier
         :second_hero => "captain America"
         :third => "Thor"
         :fourth => "hulk"
-      }
+      },
       
       :powers => {
         :iron_avenger => {}
         :super_soldier => {}
         :god => {}
         :gamma_bomb => {}
-      }
+      },
     }
   }
 end
