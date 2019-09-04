e4f8c78524ca414a9f3852a42cf426d0b3fb1106
Brian Kao <bk1613@nyu.edu>
2019-09-04 18:47:53 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,7 +14,10 @@ def monopoly_with_second_tier
   hashheroes = {
     "Avengers" => {
       :members => {
-        
+        :first_hero => "iron Man"
+        :second_hero => "captain America"
+        :third => "Thor"
+        :furth => "hulk"
       }
       
     }
